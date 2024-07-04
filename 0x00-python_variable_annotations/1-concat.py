3e064a72a811d7425fd3be2eac42ad894d68c8ef
Voxold <bilalajaha@gmail.com>
2024-07-04 20:07:41 +0000

    returns the floor of the float.

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
-"""Task 1 module.
-"""
+"""Task 1 module."""
 
 
 def concat(str1: str, str2: str) -> str:
-        """Concatenates two strings.
-            """
-                return str1 + str2
+    """Concatenates two strings."""
+    return str1 + str2
