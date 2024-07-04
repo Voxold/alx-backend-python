6d2b8226e931d0c08ad8b3fdc2f1419a331882c2
Voxold <bilalajaha@gmail.com>
2024-07-04 20:27:52 +0000

    returns their sum as a float.

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Task 5 module"""
+from typing import List
+
+
+
+def sum_list(input_list: list[float]) -> float:
+    """sums lists of floats and returns their sum"""
+    return float(sum(input_list))
