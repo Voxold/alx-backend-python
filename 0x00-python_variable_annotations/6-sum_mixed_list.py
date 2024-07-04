834751bba41af74fcc414a5a5d072165e974359a
Voxold <bilalajaha@gmail.com>
2024-07-04 21:14:26 +0000

    returns their sum as a float.

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,5 +3,5 @@
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
-    """takes a list of integers and floats and returns their sum as a float"""
+    """takes a list of integers and floats"""
     return float(sum(mxd_lst))
